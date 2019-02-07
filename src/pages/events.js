b7b1e5cb4567be6248b14a137ecafa160096afdd
Leeland Clenista <leeland_clenista@yahoo.com>
2019-02-06 20:41:44 -0800

    added react-big-calendar

--- src/pages/events.js
+++ src/pages/events.js
@@ -1,16 +1,48 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import Calendar from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import Events from '../components/events'
 
-export default props => (
-  <Layout pageProps={props}>
-    <SEO title="Events" keywords={['san diego', 'tech', 'hub', 'events', 'upcoming']} />
-    <Events events={props.data.allCalendarEvent.edges} />
-  </Layout>
-)
+const localizer = Calendar.momentLocalizer(moment);
+export default class extends React.Component {
+  state = {
+    events: [
+      {
+        start: new Date(),
+        end: new Date(moment().add(1, "days")),
+        title: "San Diego Tech Hub"
+      },
+      {
+        start: new Date(),
+        end: new Date(moment().add(2, "days")),
+        title: "Web Design Team"
+      }
+    ]
+  }
+  
+
+  render () {
+    console.log(this.props)
+    return (
+      <Layout pageProps={this.props}>
+        <SEO title="Events" keywords={['san diego', 'tech', 'hub', 'events', 'upcoming']} />
+        <Calendar
+            localizer={localizer}
+            defaultDate={new Date()}
+            defaultView="month"
+            events={this.state.events}
+            style={{ height: "100vh" }}
+          />
+          <Events events={this.props.data.allCalendarEvent.edges} />
+      </Layout>
+    )
+  }
+}
 
 export const query = graphql`
   query CALENDAR_EVENTS {
