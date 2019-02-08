67d454f712443610261bc9e51e94f85044561e29
Wilmer <pollos737@outlook.com>
2019-02-07 20:51:06 -0800

    make work with real events

--- src/pages/events.js
+++ src/pages/events.js
@@ -1,44 +1,64 @@
 import React from 'react'
 import { graphql } from 'gatsby'
-import Calendar from "react-big-calendar";
-import moment from "moment";
-import "react-big-calendar/lib/css/react-big-calendar.css";
+import Calendar from 'react-big-calendar'
+import moment from 'moment'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import Events from '../components/events'
 
-const localizer = Calendar.momentLocalizer(moment);
+// worst case scenario to style calender
+// import './styles.css'
+
+const localizer = Calendar.momentLocalizer(moment)
 export default class extends React.Component {
   state = {
     events: [
       {
         start: new Date(),
-        end: new Date(moment().add(1, "days")),
-        title: "San Diego Tech Hub"
+        end: new Date(moment().add(1, 'days')),
+        title: 'San Diego Tech Hub',
       },
       {
         start: new Date(),
-        end: new Date(moment().add(2, "days")),
-        title: "Web Design Team"
-      }
-    ]
+        end: new Date(moment().add(2, 'days')),
+        title: 'Web Design Team',
+      },
+    ],
   }
-  
 
-  render () {
-    console.log(this.props)
+  render() {
+    // console.log(moment().add(2, 'days')._d)
+    console.log(this.props.data.allCalendarEvent)
+    const realEvents = this.props.data.allCalendarEvent.edges.map(event => {
+      let start = moment(new Date(event.node.start.date || event.node.start.dateTime))
+      let end = moment(new Date(event.node.end.date || event.node.end.dateTime))
+
+      const allDay = start.format('H:mm') === end.format('H:mm')
+      if (allDay) {
+        end = end.subtract(1, 'days')
+      }
+
+      return {
+        title: event.node.summary,
+        start: start._d,
+        end: end._d,
+        allDay,
+      }
+    })
+
     return (
       <Layout pageProps={this.props}>
         <SEO title="Events" keywords={['san diego', 'tech', 'hub', 'events', 'upcoming']} />
         <Calendar
-            localizer={localizer}
-            defaultDate={new Date()}
-            defaultView="month"
-            events={this.state.events}
-            style={{ height: "100vh" }}
-          />
-          <Events events={this.props.data.allCalendarEvent.edges} />
+          localizer={localizer}
+          defaultDate={new Date()}
+          defaultView="month"
+          events={realEvents}
+          style={{ height: '100vh' }}
+        />
+        <Events events={this.props.data.allCalendarEvent.edges} />
       </Layout>
     )
   }
