6c8dd68eb801b5ad956c5cb8c0983e15d82c77aa
appleJax <appleJax@users.noreply.github.com>
2019-02-15 23:41:19 -0800

    transform event types to be consistent

--- gatsby-node.js
+++ gatsby-node.js
@@ -46,6 +46,7 @@ exports.sourceNodes = async ({ actions, createNodeId, createContentDigest }) => 
   data.items.forEach(event => {
     // console.log(event.attachments);
     const start = moment(new Date(event.start.dateTime))._d
+    const end = moment(new Date(event.end.dateTime))._d
     const eventKey = `${event.summary}-${start}`
 
     if (uniqueEvents.has(eventKey)) {
@@ -54,12 +55,17 @@ exports.sourceNodes = async ({ actions, createNodeId, createContentDigest }) => 
 
     uniqueEvents.add(eventKey)
 
+    event.start = start
+    event.end = end
+    event.title = event.summary
+    delete event.summary
+
     const nodeMeta = {
       id: createNodeId(`my-data-${event.id}`),
       parent: null,
       children: [],
       internal: {
-        type: 'CalendarEvent',
+        type: 'Event',
         content: JSON.stringify(event),
         contentDigest: createContentDigest(event),
       },
@@ -72,17 +78,43 @@ exports.sourceNodes = async ({ actions, createNodeId, createContentDigest }) => 
   return
 }
 
-exports.onCreateNode = ({ node, actions }) => {
-  const { deleteNode } = actions
+let id = 0
+exports.onCreateNode = ({
+  actions,
+  createContentDigest,
+  createNodeId,
+  node,
+}) => {
+  const { deleteNode, createNode, createParentChildLink } = actions
 
   if (node.internal.type === 'MeetupEvent') {
     const start = moment(new Date(node.time))._d
+    const end = moment(new Date(node.time + node.duration))._d
     const eventKey = `${node.name}-${start}`
 
     if (uniqueEvents.has(eventKey)) {
       deleteNode({ node })
     } else {
       uniqueEvents.add(eventKey)
+
+      const newJson = {
+        title: node.name,
+        description: node.description,
+        start,
+        end,
+      }
+      const newNode = {
+        ...newJson,
+        id: createNodeId(`${node.id}-${++id}`),
+        children: [],
+        parent: node.id,
+        internal: {
+          contentDigest: createContentDigest(newJson),
+          type: 'Event',
+        },
+      }
+      createNode(newNode)
+      createParentChildLink({ parent: node, child: newNode })
     }
   }
 }
@@ -93,65 +125,28 @@ exports.createPages = ({ graphql, actions }) => {
 
   return graphql(`
     query EVENTS {
-      allCalendarEvent {
+      allEvent {
         edges {
           node {
             id
-            summary
+            title
             description
-            location
-            end {
-              # date(formatString: "M/DD/YYYY h:mm:ss z")
-              dateTime(formatString: "M/DD/YYYY h:mm:ss z")
-            }
-            start {
-              # date(formatString: "M/DD/YYYY h:mm:ss z")
-              dateTime(formatString: "M/DD/YYYY h:mm:ss z")
-            }
-          }
-        }
-      }
-      meetupGroup {
-        name
-        link
-        description
-        next_event {
-          id
-        }
-        childrenMeetupEvent {
-          id
-          name
-          link
-          description
-          duration
-          time
-          local_date
-          local_time
-          venue {
-            name
-            lat
-            lon
-            address_1
-            city
-            state
+            start
+            end
           }
         }
       }
     }
   `).then(results => {
     if (results.errors) {
-      reject('the error', results.errors)
+      throw new Error(results.errors)
     }
 
-    const allEvents = results.data.allCalendarEvent.edges
-      .map(({ node }) => ({ ...node }))
-      .concat(results.data.meetupGroup.childrenMeetupEvent)
-
-    allEvents.forEach(event => {
+    return results.data.allEvent.edges.map(({ node }) => {
       createPage({
-        path: `/event/${event.id}`,
+        path: `/event/${node.id}`,
         component: template,
-        context: { event: event },
+        context: { event: node },
       })
     })
   })
--- src/pages/events.js
+++ src/pages/events.js
@@ -1,110 +1,41 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
-import moment from 'moment'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
-// import Events from '../components/events'
+import Events from '../components/events'
 import Calendar from '../components/calendar'
 
 function EventIndex({ data, ...props }) {
-  const googleCalendarEvents = data.allCalendarEvent.edges.map(event => {
-    const start = moment(new Date(event.node.start.dateTime))
-    const end = moment(new Date(event.node.end.dateTime))
-
-    // const allDay = start.format('H:mm') === end.format('H:mm')
-    // if (allDay) {
-    //   end = end.subtract(1, 'days')
-    // }
-
-    return {
-      id: event.node.id,
-      title: event.node.summary,
-      start: start._d,
-      end: end._d,
-      // allDay,
-      description: event.node.description,
-    }
-  })
-
-  const meetupEvents = data.meetupGroup.childrenMeetupEvent.map(event => {
-    const start = moment(new Date(event.time))
-    const end = moment(new Date(event.time + event.duration))
-    return {
-      id: event.id,
-      title: event.name,
-      description: event.description,
-      start: start._d,
-      end: end._d,
-    }
-  })
-
-  const realEvents = [
-    ...googleCalendarEvents,
-    ...meetupEvents,
-  ]
-
+  const events = data.allEvent.edges.map(({ node }) => ({ ...node }))
   return (
     <Layout pageProps={props}>
       <SEO title="Events" keywords={['san diego', 'tech', 'hub', 'events', 'upcoming']} />
 
-      <Calendar events={realEvents} />
+      <Calendar events={events} />
 
-      {/* <Events events={realEvents.slice(0, 10)} /> */}
+      <Events events={events.slice(0, 10)} />
     </Layout>
   )
 }
 
 EventIndex.propTypes = {
   data: PropTypes.shape({
-    allCalendarEvent: PropTypes.object.isRequired,
+    allEvent: PropTypes.object.isRequired,
   }).isRequired,
 }
 
 export const query = graphql`
   query EVENTS {
-    allCalendarEvent {
+    allEvent {
       edges {
         node {
           id
-          summary
+          title
           description
-          location
-          end {
-            # date(formatString: "M/DD/YYYY h:mm:ss z")
-            dateTime(formatString: "M/DD/YYYY h:mm:ss z")
-          }
-          start {
-            # date(formatString: "M/DD/YYYY h:mm:ss z")
-            dateTime(formatString: "M/DD/YYYY h:mm:ss z")
-          }
-        }
-      }
-    }
-    meetupGroup {
-      name
-      link
-      description
-      next_event {
-        id
-      }
-      childrenMeetupEvent {
-        id
-        name
-        link
-        description
-        duration
-        time
-        local_date
-        local_time
-        venue {
-          name
-          lat
-          lon
-          address_1
-          city
-          state
+          start
+          end
         }
       }
     }
