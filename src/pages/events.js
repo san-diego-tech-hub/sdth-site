7974a3b0055120186ad9f0e5855f75f37469f16c
appleJax <appleJax@users.noreply.github.com>
2019-02-16 12:24:04 -0800

    fix date format for BigCalendar

--- src/pages/events.js
+++ src/pages/events.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
+import moment from 'moment'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
@@ -8,7 +9,12 @@ import Events from '../components/events'
 import Calendar from '../components/calendar'
 
 function EventIndex({ data, ...props }) {
-  const events = data.allEvent.edges.map(({ node }) => ({ ...node }))
+  const events = data.allEvent.edges.map(({ node }) => ({
+    ...node,
+    start: moment(new Date(node.start))._d,
+    end: moment(new Date(node.end))._d,
+  }))
+
   return (
     <Layout pageProps={props}>
       <SEO title="Events" keywords={['san diego', 'tech', 'hub', 'events', 'upcoming']} />
