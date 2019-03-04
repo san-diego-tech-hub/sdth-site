031fef6dec0c93ade263ae10f4fb6d4da6696a25
appleJax <appleJax@users.noreply.github.com>
2019-03-03 22:27:15 -0800

    adjust feed background and link style

--- src/utils/hooks.js
+++ src/utils/hooks.js
@@ -10,11 +10,11 @@ export function useFormInput(initValue = "") {
 export function useCurator() {
   React.useEffect(() => {
     (function curatorIo() {
-      const i = document.createElement("script");
-      i.async = 1;
-      i.src = "https://cdn.curator.io/published/b46d667b-cd85-44f6-9f29-8991bf9f362e.js";
-      const e = document.getElementsByTagName("script")[0];
-      e.parentNode.insertBefore(i, e);
-    }());
+      const i = document.createElement("script")
+      i.async = 1
+      i.src = "https://cdn.curator.io/published/b46d667b-cd85-44f6-9f29-8991bf9f362e.js"
+      const e = document.getElementsByTagName("script")[0]
+      e.parentNode.insertBefore(i, e)
+    }())
   }, [])
 }
