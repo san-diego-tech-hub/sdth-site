141589973ebca68e4aeeddac86a0982a1d71ff10
appleJax <appleJax@users.noreply.github.com>
2019-03-12 17:23:04 -0700

    merge jhuynh tests

--- src/components/forms/__tests__/stay-connected.test.js
+++ src/components/forms/__tests__/stay-connected.test.js
@@ -1,12 +1,33 @@
 import React from "react"
 import { render } from "react-testing-library"
+import { library } from "@fortawesome/fontawesome-svg-core"
 import MockAddToMailchimp from "gatsby-plugin-mailchimp"
+import {
+  faFacebookSquare,
+  faLinkedin,
+  faSlack,
+  faTwitterSquare
+} from "@fortawesome/free-brands-svg-icons"
 import StayConnected from "../stay-connected"
+
+library.add(
+  faFacebookSquare,
+  faLinkedin,
+  faSlack,
+  faTwitterSquare
+)
 
 jest.mock("gatsby-plugin-mailchimp", () => {
   return () => Promise.resolve({
     result: "success",
     msg: "Thanks for subscribing!"
+  })
+})
+
+describe("Appearance", () => {
+  test("matches its snapshot", () => {
+    const { getByTestId } = render(<StayConnected />)
+    expect(getByTestId("stay-connected")).toMatchSnapshot()
   })
 })
 
