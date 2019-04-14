0c6011c8834fdc6105dccafdceb0eeb7f0d6854c
Aramoro <35650608+Yogo-Isoroku@users.noreply.github.com>
2019-04-14 14:17:25 -0700

    updated founder text to netlfy cms

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -28,6 +28,10 @@ const icons = {
   conversations
 }
 
+const images = {
+  claude2,
+}
+
 function About() {
   const {
     markdownRemark: { frontmatter }
@@ -38,6 +42,7 @@ function About() {
       <FounderSection>
         <h2>{frontmatter.mainTitle}</h2>
 
+
         <p>
           <img
             src={claude1}
@@ -46,60 +51,18 @@ function About() {
             width="200"
           />
         </p>
-        <p>
-          Claude Jones has a passion for creating opportunities for others to
-          succeed. This passion was shaped by the adversities he faced in his
-          youth. Not knowing his birth father, Claude was raised by his mother
-          and step-father. His upbringing was harsh, both of his parents worked
-          so he was responsible for taking care of his 3 younger siblings, he
-          faced verbal and physical abuse at home and racism in his community.
-          Feeling like the weight of the world was on his shoulders, he would
-          constantly wonder if the world would be a better place without him in
-          it.
-        </p>
-        <p>
-          When all hope seemed lost, things changed in the Spring of 1989. A
-          school counselor noticed something in Claude. He pulled him in,
-          created a supportive environment, encouraged him to believe in
-          himself, and provided a structure and example of setting and achieve
-          goals. This transformative act of acknowledgment set the tone for
-          Claude and helped shape who he is today.
-        </p>
-        <p>
-          Based on his personal experiences, Claude's philosophy in life is
-          "perseverance will help you overcome all obstacles". From a young age
-          Claude has been paving his own way, overcoming obstacles with the help
-          of mentors and coaches. From starting his first business at age 12 to
-          raise money for a video game console, to being a self-taught
-          developer, his personal motivation to better himself created
-          opportunities for him to take advantage of various situations. And
-          through his blessings he would like to pay it forward by creating
-          opportunities for others that might need a helping hand.
-        </p>
+        <Html>{frontmatter.mainDescription}</Html>
+        <div>
+          {frontmatter.mainItems.map(({ mainItem }) => {
+            const imgMain = images[mainItem.image]
+            return (
+              <div key={imgMain.image}>
+                <img src={imgMain} alt={mainItem.image} style={{ float: "right", width: "200px" }} />
 
-        <p>
-          <img
-            src={claude2}
-            style={{ float: "right" }}
-            alt="Claude Jones"
-            width="200"
-          />
-          Today, Claude Jones is a seasoned technical leader with over 15 years
-          of experience. He is currently the site lead for Walmart Labs in
-          Carlsbad helping to contribute to the growing tech culture here in San
-          Diego. In addition, Claude continues to pay it forward. In his spare
-          time he runs the Elevate Foundation, an organization he started
-          focused on giving back to the community by helping others in need. He
-          started The Practical Leadership Guy where he offers free services for
-          motivational speaking, life coaching, and his thoughts through his
-          blog to encouraging others to be the best they can be.
-        </p>
-        <p>
-          Claude sees San Diego Tech Hub as an extension of his passions to
-          continue bringing people together for the greater good. In Claude's
-          own words: "We should never wait for opportunities to come when we can
-          create them for ourselves."
-        </p>
+              </div>
+            )
+          })}
+        </div>
         <div style={{ clear: "both" }} />
       </FounderSection>
 
@@ -171,6 +134,12 @@ const aboutQuery = graphql`
     markdownRemark(frontmatter: { path: { eq: "about" } }) {
       frontmatter {
         mainTitle
+        mainDescription
+        mainItems {
+          mainItem {
+            image
+          }
+        }
         aboutTitle
         aboutDescription
         challengesTitle
