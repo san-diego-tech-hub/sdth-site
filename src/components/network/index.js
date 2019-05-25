2ea14629581d8e9715c3012af86739e95ffd7678
Sashank Pindiproli <pindiproli.sashank@gmail.com>
2019-05-25 16:31:54 -0700

    Network Page skeleton

---
+++ src/components/network/index.js
@@ -0,0 +1,39 @@
+import React from "react"
+import network from "Images/initiatives/network.png"
+import conduits from "Images/misc/conduits-flow.png"
+
+import {
+  NetworkContainer, NetworkSection, NetworkLinks
+} from "./styles"
+
+/* const icons = {
+  goals,
+  heart,
+  tasks,
+  silos,
+  judge,
+  conversations
+} */
+
+function Network() {
+  return (
+    <main>
+      <NetworkContainer>
+        <img className="conduits-flow-illustration" src={conduits} alt="Conduits Icon" />
+        <NetworkSection>
+          <img className="conduits-flow-illustration" src={conduits} alt="Conduits Icon" />
+          <img className="network-logo" src={network} alt="Network Icon" />
+        </NetworkSection>
+        <NetworkLinks>
+          <ul>
+            <li><a href="#">Resource</a></li>
+            <li><a href="#">Experience</a></li>
+            <li><a href="#">Events</a></li>
+          </ul>
+        </NetworkLinks>
+      </NetworkContainer>
+    </main>
+  )
+}
+
+export default Network
