12ed783ef03ef1d9589cb90d6e16c41c61c24fe6
appleJax <appleJax@users.noreply.github.com>
2019-07-03 00:19:37 -0700

    style search bar

--- src/components/network/index.js
+++ src/components/network/index.js
@@ -1,34 +1,21 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
 import SearchBar from "./SearchBar"
 
 import {
   NetworkContainer,
   Header,
-  Logo
+  Title
 } from "./styles"
 
 function Network() {
-  const {
-    networkLogo
-  } = useStaticQuery(networkQuery)
-
   return (
     <NetworkContainer>
       <Header>
-        <Logo fluid={networkLogo.childImageSharp.fluid} alt="SDTH Network" />
+        <Title>Network</Title>
         <SearchBar />
       </Header>
     </NetworkContainer>
   )
 }
-
-const networkQuery = graphql`
-  query NETWORK_QUERY {
-    networkLogo: file(relativePath: { eq: "network.png" }) {
-      ...childSharp
-    }
-  }
-`
 
 export default Network
