a0559961c3fa4acbf130e711bf229d96a0a7fb73
appleJax <appleJax@users.noreply.github.com>
2019-07-03 00:19:37 -0700

    style search bar

--- src/components/network/styles.js
+++ src/components/network/styles.js
@@ -1,27 +1,22 @@
 import styled from "styled-components"
 import { NAV_HEIGHT } from "Utils/constants"
-import Img from "gatsby-image"
 
 export const NetworkContainer = styled.main`
   margin: ${NAV_HEIGHT} auto;
-  max-width: 1200px;
-  padding: 0 0.5rem;
-  width: 100%;
+  padding: 0;
+  width: 100vw;
 }
 `
 
 export const Header = styled.div`
   background: linear-gradient(${props => `${props.theme.primary}, ${props.theme.primaryLight}`});
-  border-radius: 0 0 5px 5px;
   display: flex;
   flex-direction: column;
   align-items: center;
   width: 100%;
 `
 
-export const Logo = styled(Img)`
-  align-self: flex-start;
-  margin: 1rem;
-  margin-bottom: 0;
-  width: 300px;
+export const Title = styled.h1`
+  color: white;
+  text-shadow: 1px 1px 5px rgba(0,0,0,0.3);
 `
